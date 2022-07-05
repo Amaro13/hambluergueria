562351ca919566f156306ad5e58a85735812bbea
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-05 20:15:30 -0300

    added categories CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductModule } from './product/product.module';
 import { TablesModule } from './tables/tables.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule, ProductModule, TablesModule],
+  imports: [UsersModule, ProductModule, TablesModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
