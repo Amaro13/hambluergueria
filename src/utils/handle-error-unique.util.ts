84a8646ab3c1d594e70f9bd8c51d83f99462f2a2
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-09 11:47:10 -0300

    changed the text to english

--- src/utils/handle-error-unique.util.ts
+++ src/utils/handle-error-unique.util.ts
@@ -3,9 +3,9 @@ import { UnprocessableEntityException } from '@nestjs/common';
 export const handleErrorConstraintUnique = (error: Error): never => {
   const splitedMessage = error.message.split('`');
 
-  const errorMessage = `Entrada '${
+  const errorMessage = `Entry '${
     splitedMessage[splitedMessage.length - 2]
-  }' não está respeitando a constraint UNIQUE`;
+  }' not respecting the unique constraint`;
 
   throw new UnprocessableEntityException(errorMessage);
 };
