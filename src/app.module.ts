1354600470895d91f4f67553abdca8ee1558a6b9
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-12 21:59:37 -0300

    feature: created the auth module with the login resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ProductModule } from './product/product.module';
+import { ProductsModule } from './product/product.module';
 import { TablesModule } from './tables/tables.module';
 import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductModule, TablesModule, CategoriesModule],
+  imports: [UsersModule, ProductsModule, TablesModule, CategoriesModule, OrdersModule, FavoritesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
